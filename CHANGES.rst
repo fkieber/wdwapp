ba3447e4121c220438653ddb60a1615d80a20586
Frédéric KIEBER <frederic@frkb.fr>
2017-12-23 11:48:29 +0100

    Version 0.0.3

---
+++ CHANGES.rst
@@ -0,0 +1,12 @@
+Change log
+----------
+
+- **0.0.3** 2017/12/22
+
+	- Rounding results to avoid truncate warning during database update.
+    - Avoid to insert already existing weather data.
+
+- **0.0.1** 2017/12/21
+
+	First version.
+
---
+++ wdwapp/__init__.py
@@ -0,0 +1,38 @@
+__version__ = '0.0.3'
+
+def main(global_config, **settings):
+
+    from pyramid.config import Configurator
+    from sqlalchemy import engine_from_config
+    from pyramid.session import SignedCookieSessionFactory
+    from .models import DBSession, Base
+
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+    Base.metadata.bind = engine
+
+    my_session_factory = SignedCookieSessionFactory(
+        settings['session.secret'])
+
+    config = Configurator(settings=settings,
+                    session_factory=my_session_factory,
+                    root_factory='wdwapp.models.Root')
+
+    config.include('pyramid_chameleon')
+
+    # Route for cron servvice
+    config.add_route('service_get', '/cron')
+
+    # Routes for web site
+    config.add_route('wiki_view', '/')
+    config.add_route('wikipage_add', '/add')
+    config.add_route('wikipage_view', '/{uid}')
+    config.add_route('wikipage_edit', '/{uid}/edit')
+
+    # Static things
+    config.add_static_view('deform_static', 'deform:static/')
+
+    # Let's go
+    config.scan('.views')
+    config.scan('.services')
+    return config.make_wsgi_app()
